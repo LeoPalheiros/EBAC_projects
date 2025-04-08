32f26a13a113fd3fbce48f6eb7124674a47060d7
Leonardo Palheiros <leonardopalheiros@hotmail.com>
2025-04-08 15:10:31 -0300

    exercicioTS

---
+++ src/main.js
@@ -0,0 +1,11 @@
+"use strict";
+function multiplicar(a, b) {
+    return a * b;
+}
+function saudar(nome) {
+    return "Olá " + nome;
+}
+const resultadoMultiplicacao = multiplicar(2, 5);
+console.log("Resultado da multiplicação:", resultadoMultiplicacao);
+const saudacao = saudar("Leonardo");
+console.log(saudacao);
---
+++ src/main.ts
@@ -0,0 +1,13 @@
+function multiplicar(a: number, b: number): number {
+    return a * b;
+}
+
+function saudar(nome: string): string {
+    return "Olá " + nome;
+}
+
+const resultadoMultiplicacao = multiplicar(2, 5);
+console.log("Resultado da multiplicação:", resultadoMultiplicacao); 
+
+const saudacao = saudar("Leonardo");
+console.log(saudacao); 
